7511c687c80e2303092ddde815970fba191afb9f
Oleksandr Zakharchuk <zahirolek@gmail.com>
2021-02-16 12:37:51 +0200

    commit all

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 1.https://mvnrepository.com/artifact/org.jsoup/jsoup/1.13.1
 2.https://mvnrepository.com/artifact/org.apache.commons/commons-lang3/3.11
 3. https://github.com/Ignatenko2207/amazon-parser-G48
+4. https://riptutorial.com/jsoup 
+//Extract ... from HTML 
+5. https://jsoup.org/cookbook/extracting-data/dom-navigation
+6. https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind/2.11.3
+https://mvnrepository.com/artifact/com.liferay/com.fasterxml.jackson.databind/2.10.0.LIFERAY-PATCHED-1
+7. https://jsonformatter.org/
+8. 
--- src/main/java/ua/mainacademy/service/ItemPageParsingService.java
+++ src/main/java/ua/mainacademy/service/ItemPageParsingService.java
@@ -1,54 +1,97 @@
 package ua.mainacademy.service;
 
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import ua.mainacademy.model.Item;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 public class ItemPageParsingService {
 
-    public static Item getItemFromPage(String url){
+    public static Item getItemFromPage(String url) {
 
-        Document document=DocumentExtractorService.getDocument(url);
-        Element element=document.getElementById("ppd");
+        Document document = DocumentExtractorService.getDocument(url);
+        Element productBlock = document.getElementById("dp-container");
 
 
-        String name=extractName(element);
-        String code=extractCode(url);
-        int price=extractPrice(element);
-        int initPrice=extractInitPrice(element)==0 ? price : extractInitPrice(element);
-//        String imageUrl=extractImageUrl(element);
-//        String group=extractGroup(document);
-//        String seller=extractSeller(element);
+        String name = extractName(productBlock);
+        String code = extractCode(url);
+        int price = extractPrice(productBlock);
+        int initPrice = extractInitPrice(productBlock) == 0 ? price : extractInitPrice(productBlock);
+        String imageUrl = extractImageUrl(productBlock);
+        String group = extractGroup(productBlock);
+        String seller = extractSeller(productBlock);
 
         return Item.builder()
                 .code(code)
                 .name(name)
                 .price(price)
                 .initPrice(initPrice)
-               // .group(group)
+                .imageUrl(imageUrl)
                 .url(url)
-               // .imageUrl(imageUrl)
-               // .seller(seller)
+                .group(group)
+                .seller(seller)
                 .build();
 
     }
 
-    private static int extractInitPrice(Element element) {
-        List<Element>elementList = element.getElementsByAttributeValueStarting("class", "a-size-base a-color-secondary");
-        if (elementList.isEmpty()){
-            return 0;
-        }
-        return Integer.valueOf(elementList.get(0).text().replaceAll("\\D",""));
+    private static String extractSeller(Element element) {
+        Element sellerElement = element.getElementById("tabular-buybox-truncate-1").getElementsByClass("tabular-buybox-text").first();
+        return sellerElement.text();
+
     }
 
-    // using jsoup from mvn repository for all code
+    private static String extractGroup(Element document) {
+        String result ="";
+        Element groupDiv=document.getElementById("wayfinding-breadcrumbs_feature_div");
+        Elements groupElements=groupDiv.getElementsByTag("a");
+        List<String>groups=new ArrayList<>();
+        for (Element element :groupElements ){
+        groups.add(element.text());
+        }
+        return StringUtils.join(groups, ">");
+    }
+
+    private static String extractImageUrl(Element element) {
+        Element imageElement = element.getElementById("landingImage");
+        String imageLinks = imageElement.attr("data-a-dynamic-image");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            Map result = objectMapper.readValue(imageLinks, Map.class);
+            return (String) result.keySet().iterator().next();
+
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return StringUtils.substringBefore(StringUtils.substringAfter(imageLinks, "{\""), "\":");
+
+    }
+
+    private static int extractInitPrice(Element element) {
+        List<Element> elementList = element.getElementsByAttributeValueStarting("class", "a-size-base a-color-secondary");
+        if (elementList.isEmpty()) {
+            return 0;
+        }
+        return Integer.valueOf(elementList.get(0).text().replaceAll("\\D", ""));
+    }
+
+//     using jsoup from mvn repository for all code
 
     private static int extractPrice(Element element) {
-        String priseRow= element.getElementById("priceblock_ourprice").text();
-        return Integer.valueOf(priseRow.replaceAll("\\D",""));
+        String priseRow = element.getElementById("priceblock_ourprice").text();
+        return Integer.valueOf(priseRow.replaceAll("\\D", ""));
     }
 //using string utils (apache commons lens) from mvn repository
 
